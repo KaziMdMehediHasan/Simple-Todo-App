597f60dc362010667667d50415a5b66b9605b378
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-06-28 10:50:47 +0600

    basic redux setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-function App() {
+import { Button } from "./components/ui/button"
 
+function App() {
   return (
     <>
       <h1>Hello from New App</h1>
+      <Button>My Button</Button>
     </>
   )
 }
